72e82cd4451a4efac0aeed0299c808f677329980
nacbotics <xavierroymccoy@gmail.com>
2022-02-17 06:24:02 +0100

    just added postgress as db

--- MyWatchlist/local_settings.py
+++ MyWatchlist/local_settings.py
@@ -9,8 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-import os
-import django_heroku
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -24,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_t_1jv9yl35%ud&4kl^wevy4=#a#dd%4rf_u-=%^z7vnmf8!q9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["https://vigilisbot.herokuapp.com",]
+ALLOWED_HOSTS = ["*",]
 
 WEBHOOK_TOKEN = "1928565537:AAEUG6GAPoWZm3zcDuCkjal3kA-kgKCEbSA"
-WEBHOOK_URL = "https://vigilisbot.herokuapp.com/"
+WEBHOOK_URL = "https://3948-105-112-104-26.ngrok.io/"
 
 
 # Application definition
@@ -79,62 +78,13 @@ WSGI_APPLICATION = 'MyWatchlist.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'd591rj88dd9v39', 
-        'USER': 'kgnjscnwukodcs', 
-        'PASSWORD': 'ba6e8b02a06ec568a607e124aebd399e408d4ecb6c5e8eae7755e8b73b30f8dc',
-        'HOST': '    postgres://kgnjscnwukodcs:ba6e8b02a06ec568a607e124aebd399e408d4ecb6c5e8eae7755e8b73b30f8dc@ec2-54-195-76-73.eu-west-1.compute.amazonaws.com:5432/d591rj88dd9v39', 
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
-#LOGGING
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'verbose': {
-            'format': ('%(asctime)s [%(process)d] [%(levelname)s] ' +
-                       'pathname=%(pathname)s lineno=%(lineno)s ' +
-                       'funcname=%(funcName)s %(message)s'),
-            'datefmt': '%Y-%m-%d %H:%M:%S'
-        },
-        'simple': {
-            'format': '%(levelname)s %(message)s'
-        }
-    },
-    'handlers': {
-        'null': {
-            'level': 'DEBUG',
-            'class': 'logging.NullHandler',
-        },
-        'console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-            'formatter': 'verbose'
-        }
-    },
-    'loggers': {
-        'testlogger': {
-            'handlers': ['console'],
-            'level': 'INFO',
-        }
-    }
-}
-
-DEBUG_PROPAGATE_EXCEPTIONS = True
-COMPRESS_ENABLED = os.environ.get('COMPRESS_ENABLED', False)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -169,25 +119,12 @@ USE_L10N = True
 USE_TZ = True
 
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.2/howto/static-files/
+
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-
-# STATIC_URL = '/static/' 
-# STATICFILES_DIRS = [ os.path.join(BASE_DIR, '/app/static'), ] 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'collectstatic') 
-
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage.manifest_strict'#storage.StaticFilesStorage'
-
-# Activate Django-Heroku.
-django_heroku.settings(locals())
