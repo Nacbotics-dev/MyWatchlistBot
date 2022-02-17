a5ec41cdc571b415e12450db1423dd562b10fa09
nacbotics <xavierroymccoy@gmail.com>
2022-02-17 07:20:14 +0100

    Added the static url

--- MyWatchlist/urls.py
+++ MyWatchlist/urls.py
@@ -1,10 +1,12 @@
 """Vigilis BOT URL Configuration"""
 
 from django.contrib import admin
-
-from django.urls import path
+from django.conf import settings
+from django.urls import path,re_path
 from Watchlist.views import WebConnect
 from django.conf.urls import url,include
+from django.conf.urls.static import static
+from django.views.static import serve
 from MyWatchlist.settings import WEBHOOK_TOKEN
 
 
@@ -12,4 +14,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     url(r''+WEBHOOK_TOKEN,WebConnect,),
     url(r'',WebConnect,),
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
